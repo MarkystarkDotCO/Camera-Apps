b31c24030b21e8020692d34878f250abc2498193
O(logN) <48640302+MarkystarkDotCO@users.noreply.github.com>
2021-03-21 01:46:36 +0700

    Add files via upload

---
+++ src/main/java/com/example/owashita/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.owashita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.provider.MediaStore;
+import android.view.View;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void takePhoto(View view){
+        dispatchTakePictureIntent();
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+}
